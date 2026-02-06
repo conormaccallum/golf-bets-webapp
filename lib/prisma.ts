38fe889a85f0c00e95628bc7e1ba9b94d63d1d1d
Conor MacCallum <conor.maccallum@gmail.com>
2026-02-06 12:38:26 +0000

    Force fix prisma import path everywhere

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../webapp/generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
